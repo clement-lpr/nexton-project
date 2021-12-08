7135a3bf2829dd5c6e686c212dd0c4f31aaddefe
Wajdi BELHADJ-ALI <belhadjali.wajdi@gmail.com>
2021-12-08 17:46:50 +0100

    swagger-first-commit

--- src/clients/controllers/clients.controller.ts
+++ src/clients/controllers/clients.controller.ts
@@ -2,6 +2,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Client } from '../schemas/client.model';
 import { ClientsRepository } from '../repositories/clients.repository';
+import { ApiParam } from '@nestjs/swagger';
 
 @Controller('clients')
 export class ClientsController {
@@ -16,6 +17,7 @@ export class ClientsController {
     }
 
   @Get(':buId')
+  @ApiParam({ name: 'Business unit Id', required: true})
   async findAllClients(@Param('buId') buId) {
         return await this.clientsRepository.getClientsByBu(buId);
   }
--- src/clients/repositories/clients.repository.ts
+++ src/clients/repositories/clients.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Client } from '../schemas/client.model';
 
 @Injectable()
@@ -13,7 +13,7 @@ export class ClientsRepository {
       return result;
     }
 
-    async getClientsByBu(buId: string){
+    async getClientsByBu(buId: ObjectId){
       const result = await this.clientModel.find({ businessUnit: buId })
       .populate('businessUnit')
       .exec()
